a563eaf3e7e6ac8db9bc8591a0cffd1d842ce6ea
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-13 20:52:01 +0300

    Initial Commit

---
+++ classConcept4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  classConcept4
+//
+//  Created by Kaan Şengün on 13.11.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

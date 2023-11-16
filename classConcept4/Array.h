e543b46adffb7cf72c2be7d0744c2526f8afa987
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-16 22:23:35 +0300

    Finish

--- classConcept4/Array.h
+++ classConcept4/Array.h
@@ -9,91 +9,92 @@
 using namespace std;
 
 class Array{
-private:
+protected:
     int *data,size,capacity;
     int findIndex(int number, int b=0);
 public:
-    Array(int user_capacity=5);
-    ~Array();
-    Array(const Array &copyArray);
+    Array(int user_capacity=5){
+        capacity=user_capacity;
+        size=0;
+        data=new int[capacity];
+        cout<<"Array is created with capacity "<<capacity<<endl;
+    }
+    ~Array(){
+        delete[] data;
+        cout<<"Array destroyed."<<endl;
+    }
+    Array(const Array &copyArray){
+        capacity=copyArray.capacity;
+        size=copyArray.size;
+        data=new int[capacity];
+        for(int i=0;i<copyArray.size;i++){
+            data[i]=copyArray.data[i];
+        }
+        cout<<"Array copied to target."<<endl;
+    }
     int getSize() const{
         return size;
     }
     int getCapacity() const{
         return capacity;
     }
-    void AddItem(int number);
-    void printItems();
-    void removeIndexItem(int index);
-    void removeItem(int number);
-    void findElement(int number, int b=0);
-};
-Array::Array(int user_capacity){
-    capacity=user_capacity;
-    size=0;
-    data=new int[capacity];
-    cout<<"Array is created with capacity "<<capacity<<endl;
-    
-}
-Array::~Array(){
-    delete[] data;
-    cout<<"Array destroyed."<<endl;
-}
-Array::Array(const Array &copyArray){
-    capacity=copyArray.capacity;
-    size=copyArray.size;
-    data=new int[capacity];
-    for(int i=0;i<copyArray.size;i++){
-        data[i]=copyArray.data[i];
-    }
-    cout<<"Array copied to target."<<endl;
-}
-void Array::AddItem(int number){
-    if(size==capacity){
-        int *temp_data=new int[capacity*2];
-        for(int i=0;i<size;i++){//önceki diziden geçici dizeye elemanlar aktarıldı
-            temp_data[i]=data[i];
-        }
-        delete[] data;
-        data=temp_data;
-        capacity*=2;
-    }
-    data[size]=number;
-    size++;
-}
-void Array::printItems(){
-    if(size!=0){
-        for(int i=0;i<size;i++)
-            cout<<data[i]<<"-";
-    }
-    cout<<"\n"<<"capacity:"<<capacity<<" size:"<<size<<endl;
-}
-void Array::removeIndexItem(int index){
-    if(index<0||index>=size){
-        cout<<"ERROR! No item at the index"<<index<<endl;
-        return;
-    }
-    int removeItem=data[index];
-    for(int i=index;i<size;i++)
-        data[i]=data[i+1];
-    size-=1;
-    cout<<"Item "<<removeItem<< " is deleted"<<endl;
-    
-}
-void Array::removeItem(int number){
-    int counter=0;
-    for(int i=0;i<=size;i++){
-        if(data[i]==number){
-            for(int j=i;j<size;j++){
-                data[j]=data[j+1];
+    void addItem(int number){
+        if(size==capacity){
+            int *temp_data=new int[capacity*2];
+            for(int i=0;i<size;i++){//önceki diziden geçici dizeye elemanlar aktarıldı
+                temp_data[i]=data[i];
             }
-            counter+=1;
-            i-=1;
+            delete[] data;
+            data=temp_data;
+            capacity*=2;
+        }
+        data[size]=number;
+        size++;
+    }
+    void printItems(){
+        if(size!=0){
+            for(int i=0;i<size;i++)
+                cout<<data[i]<<"-";
+        }
+        cout<<"\n"<<"capacity:"<<capacity<<" size:"<<size<<endl;
+    }
+    void removeIndexItem(int index){
+        if(index<0||index>=size){
+            cout<<"ERROR! No item at the index"<<index<<endl;
+            return;
+        }
+        int removeItem=data[index];
+        for(int i=index;i<size;i++)
+            data[i]=data[i+1];
+        size-=1;
+        cout<<"Item "<<removeItem<< " is deleted"<<endl;
+    }
+    void removeItem(int number){
+        int counter=0;
+        for(int i=0;i<=size;i++){
+            if(data[i]==number){
+                for(int j=i;j<size;j++){
+                    data[j]=data[j+1];
+                }
+                counter+=1;
+                i-=1;
+            }
+        }
+        cout<<counter<<" items ("<<number<<") "<<"are deleted from the array"<<endl;
+        size-=counter;
+    }
+    void findElement(int number, int index=0){
+        int result=findIndex(number,index);
+        if(result!=-1){
+            cout<<number<<" found at index "<<result<<endl;
+        }
+        else{
+            cout<<number<<" not found in the array"<<endl;
         }
     }
-    cout<<counter<<" items ("<<number<<") "<<"are deleted from the array"<<endl;
-    size-=counter;
-}
+};
+
+
 int Array::findIndex(int number,int index){
     for(int i=index;i<size;i++){
         if(data[i]==number){
@@ -102,13 +103,42 @@ int Array::findIndex(int number,int index){
     }
     return -1;
 }
-void Array::findElement(int number,int index){
-    int result=findIndex(number,index);
-    if(result!=-1){
-        cout<<number<<" found at index "<<result<<endl;
+class unsortedArray: public Array{
+public:
+    unsortedArray(int user_capacity=5):Array(user_capacity){};
+    unsortedArray(const unsortedArray& oth):Array(oth){};
+    void printItems(){
+        cout<<"Unsorted Array:";
+        Array::printItems();
     }
-    else{
-        cout<<number<<" not found in the array"<<endl;
+};
+class sortedArray: public Array{
+public:
+    sortedArray(int user_capacity=5):Array(user_capacity){};
+    sortedArray(const sortedArray& oth):Array(oth){};
+    void addItem(int number){
+        if(capacity==size){
+            int *temp_data=new int[capacity*2];
+            for(int i=0;i<size;i++){
+                temp_data[i]=data[i];
+            }
+            delete [] data;
+            data=temp_data;
+            capacity*=2;
+        }
+        int i=size - 1; // Yeni elemanın yerleştirileceği indeks
+            while(i>=0&&data[i] > number){
+                data[i+1] = data[i]; // Elemanları sağa kaydır
+                i--;
+            }
+            data[i+1]=number; // Yeni elemanı uygun konuma yerleştir
+            size++;
+        
     }
-}
+    void printItems(){
+        cout<<"Sorted Array:";
+        Array::printItems();
+    }
+};
+
 
--- classConcept4/main.cpp
+++ classConcept4/main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 
 int main(int argc, const char * argv[]) {
-   
+    /*
     Array mainArray(10);
     Array mainArray2(mainArray);
     cout<<"Capacity:"<<mainArray.getCapacity()<<endl;
@@ -20,26 +20,26 @@ int main(int argc, const char * argv[]) {
     cout<<"Capacity:"<<mainArray2.getCapacity()<<endl;
     cout<<"Size:"<<mainArray2.getSize()<<endl;
  
-    mainArray.AddItem(5);
-    mainArray.AddItem(12);
-    mainArray.AddItem(12);
-    mainArray.AddItem(12);
-    mainArray.AddItem(12);
-    mainArray.AddItem(23);
-    mainArray.AddItem(92);
-    mainArray.AddItem(3);
-    mainArray.AddItem(85);
-    mainArray.AddItem(76);
-    mainArray.AddItem(32);
-    mainArray.AddItem(3);
+    mainArray.addItem(5);
+    mainArray.addItem(12);
+    mainArray.addItem(12);
+    mainArray.addItem(12);
+    mainArray.addItem(12);
+    mainArray.addItem(23);
+    mainArray.addItem(92);
+    mainArray.addItem(3);
+    mainArray.addItem(85);
+    mainArray.addItem(76);
+    mainArray.addItem(32);
+    mainArray.addItem(3);
     mainArray.printItems();
-    mainArray.AddItem(42);
+    mainArray.addItem(42);
     mainArray.printItems();
     
     Array mainArray3=mainArray;
     mainArray3.printItems();
-    mainArray3.AddItem(15);
-    mainArray3.AddItem(18);
+    mainArray3.addItem(15);
+    mainArray3.addItem(18);
     
     mainArray.printItems();
     mainArray3.printItems();
@@ -51,9 +51,25 @@ int main(int argc, const char * argv[]) {
     mainArray3.printItems();
     
     mainArray3.findElement(12,2);
+   */
+    unsortedArray usArray(10);
+    sortedArray sArray(10);
     
+    usArray.addItem(10);
+    usArray.addItem(8);
+    usArray.addItem(12);
+    usArray.addItem(7);
+    usArray.addItem(150);
+    usArray.addItem(5);
+    usArray.printItems();
     
-    
+    sArray.addItem(10);
+    sArray.addItem(8);
+    sArray.addItem(12);
+    sArray.addItem(7);
+    sArray.addItem(150);
+    sArray.addItem(5);
+    sArray.printItems();
     
     
     return 0;
